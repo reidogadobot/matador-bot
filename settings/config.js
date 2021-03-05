0f0282f007aa08df61eb04adce43f661184fa974
reidogadobot <netoff197@gmail.com>
2021-03-05 07:30:55 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'SHAOLIN_BOT',
+        ownerName: 'shaolin',
+        youtube: 'https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g',
+        instagram: 'https://instagram.com/animexx_png?igshid=e7zziiwn3d9y',
+}
